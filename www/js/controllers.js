4850b0f37ed8eaf5206e7ba7a213548cc133d2b3
Dennis Dunn <ansofive@gmail.com>
2016-01-03 21:57:39 -0500

    Modal edit is in place.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,23 +39,63 @@ angular.module('app.controllers', [])
         }
     })
 
-    .controller('PatientCtrl', function ($scope, $stateParams, PatientService) {
+    .controller('PatientCtrl', function ($scope, $stateParams, $ionicModal, PatientService) {
 
-        PatientService
-            .read($stateParams.patientId)
-            .then(function (result) {
-                $scope.vm = result;
-            });
+        if ($stateParams.patientId) {
+            PatientService
+                .read($stateParams.patientId)
+                .then(function (result) {
+                    $scope.vm = result;
+                });
+        } else {
+            $scope.vm = null;
+        }
+
+        $scope.isEditing = false;
+
+        $ionicModal.fromTemplateUrl('templates/patient-edit.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
         $scope.startEdit = function () {
-        };
-
-        $scope.cancelEdit = function () {
+            $scope.isEditing = true;
+            $scope.modal.show();
         };
 
         $scope.finishEdit = function () {
-            PatientService.save($scope.vm);
+            PatientService
+                .save($scope.vm)
+                .then(function () {
+                    $scope.modal.hide();
+                });
         };
+
+        $scope.cancelEdit = function () {
+            PatientService
+                .read($scope.vm._id)
+                .then(function (result) {
+                    $scope.vm = result;
+                    $scope.modal.hide();
+                });
+        };
+        
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
+        });
+        
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function () {
+            $scope.isEditing = false;
+        });
+        
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function () {
+            $scope.isEditing = false;
+        });
     })
 
     .controller('SettingsCtrl', function ($scope, DbSetup) {
